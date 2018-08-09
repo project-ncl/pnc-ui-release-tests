c904e24c31806bad7a73e4bd90d9304cc05b2f83
Alex Creasy <acreasy@redhat.com>
2018-08-09 16:29:43 +0100

    Improved way params are handled

--- protractor.config.js
+++ protractor.config.js
@@ -3,11 +3,13 @@ const config = require('./src/config');
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: config.seleniumAddress,
+    seleniumServerJar: config.seleniumServerJar,
+    chromeDriver: config.chromeDriver,
     capabilities: {
         'browserName': 'chrome'
     },
     suites: {
         release: 'tests/release/*.spec.js',
-        environment: 'tests/environment.spec.js'
+        integration: 'tests/integration/*.spec.js'
     }
 };
